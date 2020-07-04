6a08b273828260964da5a0520a33c3a334b16242
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-07-04 10:59:14 +0200

    Add cache options

--- apis_scraper.py
+++ apis_scraper.py
@@ -1,3 +1,5 @@
+import os
+import time
 from time import sleep
 
 import requests
@@ -34,7 +36,7 @@ def _get_sup(th):
     return [float(parts[0]), growth]
 
 
-def scrape():
+def scrape(no_cache=False, cache_file_name='wyniki.json'):
     result = {
         'success': False,
         'support': {
@@ -55,14 +57,34 @@ def scrape():
         }
     }
 
-    print('Getting the site...')
-    res = _get_site()
-    if res is None:
-        print("Can't get the site! Most probably no internet :/")
-        return result
+    get_site = False
+    try:
+        if no_cache:
+            raise ValueError
+        modify = os.path.getmtime(cache_file_name)
+        if modify < time.time() - 24 * 60 * 60:  # If cache file is older than 24h
+            raise IOError
+        with open(cache_file_name, 'rb') as f:
+            soup = BeautifulSoup(f, 'html.parser')
+    except FileNotFoundError:
+        print('Cache file not found!')
+        get_site = True
+    except IOError:
+        print('Cache file toot old!')
+        get_site = True
+    except ValueError:
+        print('No-cache set to true, not touching cache files!')
+        get_site = True
+
+    if get_site:
+        print('Getting the site from internet...')
+        res = _get_site()
+        if res is None:
+            print("Can't get the site! Most probably no internet :/")
+            return result
+        soup = BeautifulSoup(res.content, 'html.parser')
 
     print('Parsing with soup...')
-    soup = BeautifulSoup(res.content, 'html.parser')
 
     div = soup.find('div', class_='entry-content clearfix')
     table = div.find('table')
